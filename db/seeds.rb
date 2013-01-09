bd4dc2877dc1044a6e2661b68fe1babade5b3cec
Brett Hoyer <jbh5079@gmail.com>
2013-01-08 22:56:07 -0500

    passing exercise id to turn form and making fields hidden

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+User.create(:email => 'brett@example.com',
+			:name => 'Brett Hoyer',
+			:password => '1',
+			:password_confirmation => '1')
+
+Exercise.delete_all
+Exercise.create(:name => 'Bench Press',
+				:date => Date.today,
+				:user_id => User.last.id)
+
+Exercise.create(:name => 'Squats',
+				:date => Date.today,
+				:user_id => User.last.id)
+
+Turn.delete_all
+Turn.create(:date => Date.today, 
+			:reps => 12, 
+			:turn_num => 1, 
+			:weight => 200, 
+			:exercise_id => Exercise.where(:name => 'Bench Press'))
