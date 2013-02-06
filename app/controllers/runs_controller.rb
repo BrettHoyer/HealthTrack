88d1c7423992ebbb92cfaa2aba3c9075343b5c53
Brett Hoyer <jbh5079@gmail.com>
2013-02-06 11:39:31 -0500

    stylistic changes to exercise index page to limit use of bootstrap, creating custom buttons and styling charts

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -44,7 +44,7 @@ class RunsController < ApplicationController
 
     respond_to do |format|
       if @run.save
-        format.html { redirect_to @run, notice: 'Run was successfully created.' }
+        format.html { redirect_to runs_url, notice: 'Run was successfully created.' }
         format.json { render json: @run, status: :created, location: @run }
       else
         format.html { render action: "new" }
