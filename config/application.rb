7e31716e95c55bb69a93ad7e6630ab5d91bd8f81
Brett Hoyer <jbh5079@gmail.com>
2013-02-21 21:23:46 -0500

    another test

--- config/application.rb
+++ config/application.rb
@@ -60,5 +60,7 @@ module Meathead
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.compile = true
+
   end
 end
