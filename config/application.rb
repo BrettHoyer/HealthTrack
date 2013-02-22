640bdc1c62b4f32dd2ef729da0bd7ebe8e73a91b
Brett Hoyer <jbh5079@gmail.com>
2013-02-21 21:04:29 -0500

    working on heroku issues

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,7 @@ module Meathead
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
   end
 end
