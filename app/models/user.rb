78324d65a0e7ed93497471b8a84eb7d46d3b7cad
Brett Hoyer <jbh5079@gmail.com>
2013-02-23 16:21:10 -0500

    updating login/signup styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ActiveRecord::Base
   has_many :exercises
   has_many :turns, :through => :exercises
 
+  before_save { |user| user.email = user.email.downcase }
+
   has_secure_password
 end
